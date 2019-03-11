b1a41fd8287f051fce9a0dc32e88728728b870b5
Serj <postwork@mail.ru>
2019-03-11 14:16:38 +0400

    Переделка табличной вёрстки на Grid Bootstrap 4

--- index.php
+++ index.php
@@ -12,50 +12,80 @@
 </head>	
 
 <body>
+
+           
     <h4 class="text-center">Калькулятор</h4>
-       <form name="calc" id="calculator">
-            <table class="table table-borderless container">
-              <tbody>
-               <tr>
-                <td colspan="4">
-                    <div class="form-group col-md-13">
-<!--                        <input type="text" class="form-control" rows="2" id="digitalArea">-->
-                       <textarea disabled readonly class="form-control" id="exampleFormControlTextarea1" rows="2"></textarea>
-                    </div>
-                </td>
-                </tr>
-                <tr>
-                <tr>
-                  <td><button type="submit" class="btn btn-primary">C</button></td>
-                  <td><button type="submit" class="btn btn-primary">&larr;</button></td>
-                  <td><button type="submit" class="btn btn-primary">/</button></td>
-                  <td><button type="submit" class="btn btn-primary">х</button></td>
-                </tr>
-                <tr>
-                  <td><button type="submit" class="btn btn-primary">7</button></td>
-                  <td><button type="submit" class="btn btn-primary">8</button></td>
-                  <td><button type="submit" class="btn btn-primary">9</button></td>
-                  <td><button type="submit" class="btn btn-primary">&ndash;</button></td>
-                </tr>
-                <tr>
-                  <td><button type="submit" class="btn btn-primary">4</button></td>
-                  <td><button type="submit" class="btn btn-primary">5</button></td>
-                  <td><button type="submit" class="btn btn-primary">6</button></td>
-                  <td><button type="submit" class="btn btn-primary">+</button></td>
-                </tr>
-                <tr>
-                  <td><button type="submit" class="btn btn-primary">1</button></td>
-                  <td><button type="submit" class="btn btn-primary">2</button></td>
-                  <td><button type="submit" class="btn btn-primary">3</button></td>
-                  <td rowspan="4"><button type="submit" class="btn btnEnter btn-primary">=</button></td>
-                </tr>
-                <tr>
-                  <td colspan="2"><button type="submit" class="btn btnZero btn-primary">0</button></td>
-                  <td><button type="submit" class="btn btn-primary">,</button></td>
-                </tr>
-              </tbody>
-            </table>
-        </form>
-                    
+    <form name="calc" id="calculator">
+        <div class="container col-xs-6 col-sm-4 col-md-3 col-lg-2">
+            <div class="form-group col-md-13">
+               <textarea disabled readonly class="form-control" id="exampleFormControlTextarea1" rows="2"></textarea>
+            </div>
+            <div class="row" style="max-width: 200px;">
+                <div class="col-3">
+                    <button type="submit" class="btn btn-primary btn-danger">C</button>
+                </div>
+                <div class="col-3">
+                    <button type="submit" class="btn btn-danger">&larr;</button>
+                </div>
+                <div class="col-3">
+                    <button type="submit" class="btn btn-dark">/</button>
+                </div>
+                <div class="col-3">
+                    <button type="submit" class="btn btn-dark">х</button>
+                </div>
+            </div>
+            <div class="row" style="max-width: 200px;">
+                <div class="col-3">
+                    <button type="submit" class="btn btn-secondary">7</button>
+                </div>
+                <div class="col-3">
+                    <button type="submit" class="btn btn-secondary">8</button>
+                </div>
+                <div class="col-3">
+                    <button type="submit" class="btn btn-secondary">9</button>
+                </div>
+                <div class="col-3">
+                    <button type="submit" class="btn btn-dark">&ndash;</button>
+                </div>
+            </div>
+            <div class="row" style="max-width: 200px;">
+                <div class="col-3">
+                    <button type="submit" class="btn btn-secondary">4</button>
+                </div>
+                <div class="col-3">
+                    <button type="submit" class="btn btn-secondary">5</button>
+                </div>
+                <div class="col-3">
+                    <button type="submit" class="btn btn-secondary">6</button>
+                </div>
+                <div class="col-3">
+                    <button type="submit" class="btn btn-dark">+</button>
+                </div>
+            </div>
+            <div class="row" style="max-width: 200px;">
+                <div class="col-3">
+                    <button type="submit" class="btn btn-secondary">1</button>
+                </div>
+                <div class="col-3">
+                    <button type="submit" class="btn btn-secondary">2</button>
+                </div>
+                <div class="col-3">
+                    <button type="submit" class="btn btn-secondary">3</button>
+                </div>
+                <div class="col-3">
+                    <button type="submit" class="btn btnEnter btn-dark">=</button>
+                </div>
+            </div>
+            <div class="row align-items-start" style="max-width: 200px;">
+                <div class="clearfix"></div>  
+                   <div class="col-6">
+                    <button type="submit" class="btn btnZero btn-secondary">0</button>
+                </div>
+                <div class="col-3">
+                    <button type="submit" class="btn btn-secondary">,</button>
+                </div>
+            </div>
+        </div>
+    </form>                                                                                                                                  
 </body>
 </html>
